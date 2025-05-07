e50638be311f9abc60648c336acbf3949f9aa57a
jahirusher <jahirusher2@icloud.com>
2025-05-07 13:31:21 -0400

    updated cleanDB.ts

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,13 +3,16 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray()
+    const model = models[modelName];
+    if (model?.db?.db) {
+      let modelExists = await models[modelName].db.db.listCollections({
+        name: collectionName
+      }).toArray();
 
-    if (modelExists.length) {
-      await db.dropCollection(collectionName);
-    }
+      if (modelExists?.length) {
+        await db.dropCollection(collectionName);
+      }
+    } else { console.log(`Model ${modelName} does not exist in the database`); }
   } catch (err) {
     throw err;
   }
