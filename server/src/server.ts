ff8a7d4464e75fc98b9d3b9e86c8ecf00dbd8017
jahirusher <jahirusher2@icloud.com>
2025-05-09 20:24:32 -0400

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,7 +16,7 @@ app.use('/api', routes);
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 
-app.get('*', (req, res) => {
+app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
