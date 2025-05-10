ac2aa92fd3cc0edda20cffb3dec195c10bf9ece2
jahirusher <jahirusher2@icloud.com>
2025-05-09 20:53:25 -0400

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -17,6 +17,10 @@ app.use('/api', routes);
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
