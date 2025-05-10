208829b97f6dc9b8bfb30243257935b86d2977fa
jahirusher <jahirusher2@icloud.com>
2025-05-09 21:18:11 -0400

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', 
   server: {
     port: 3000,
     open: true,
@@ -16,3 +17,4 @@ export default defineConfig({
     },
   },
 });
+
